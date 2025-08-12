9a93a9a462db1615390d64a62e72baa349e69bb7
Allan Henrique Kamimura <allan.kamimura@toradex.com>
2025-08-12 17:04:01 -0300

    expose the correct WSGI server to vercel
    Signed-off-by: Allan Henrique Kamimura <allan.kamimura@toradex.com>

--- app/index.py
+++ app/index.py
@@ -2,11 +2,11 @@ from app.callbacks import register_callbacks
 from dash import Dash
 from app.layout import create_layout
 
-app = Dash(__name__)
-app.layout = create_layout()
+dash_app = Dash(__name__)
+dash_app.layout = create_layout()
+register_callbacks(dash_app)
 
-# register_callbacks(app, df_inkeep, df_discourse)
-register_callbacks(app)
+app = dash_app.server  # Expose the WSGI app for Vercel
 
 if __name__ == "__main__":
-    app.run(port=8080, dev_tools_ui=True, debug=True, host="127.0.0.1")
+    dash_app.run(port=8080, dev_tools_ui=True, debug=True, host="127.0.0.1")
